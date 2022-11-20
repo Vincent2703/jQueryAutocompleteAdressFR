0ac24a1daa2d2fea27e8ca0db867b8ea920dd71f
Vincent Bourdon <vincent.bourdon41@gmail.com>
2022-11-20 09:42:53 +0100

    Use normalize js function
    Better way to normalize (removing diacritics) the query

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
 jQuery(document).ready(function(){
-    var accentMap = {
-      "à": "a",
-      "â": "a",
-      "é": "e",
-      "è": "e",
-      "ê": "e",
-      "ë": "e",
-      "ï": "i",
-      "î": "i",
-      "ô": "o",
-      "ö": "o",
-      "û": "u",
-      "ù": "u",
-      "À": "A",
-      "Â": "A",
-      "É": "E",
-      "È": "E",
-      "Ê": "E",
-      "Ë": "E",
-      "Ï": "I",
-      "Î": "I",
-      "Ô": "O",
-      "Ö": "O",
-      "Û": "U",
-      "Ù": "U"
-    };
-    function normalize(value) {
-        let res = '';
-        for (let i = 0; i < value.length; i++) {
-			res += accentMap[value.charAt(i)] || value.charAt(i);
-        }
-        return res;
-    };
- 
     jQuery("#address").keyup(function(){
         if(jQuery(this).val() !== '') {
             var getValue = encodeURIComponent(jQuery(this).val());
@@ -50,8 +16,8 @@ jQuery(document).ready(function(){
                         source: function(request, response) {
                             var matcher = new RegExp("^(?=.*\\b" + request.term.trim().split(/\s+/).join("\\b)(?=.*\\b") + ").*$", 'i' );
                             response(jQuery.grep(labels, function(value) {
-								value = value.label || value.value || value;
-								return matcher.test(value) || matcher.test(normalize(value));
+				value = value.label || value.value || value;
+				return matcher.test(value) || matcher.test(value.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
                             }));
                         },
                         minLength: 5,
