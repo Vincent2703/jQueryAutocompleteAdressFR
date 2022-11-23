1984459d0367dcf84c6569ef792b2ff775abdcbe
Vincent Bourdon <vincent.bourdon41@gmail.com>
2022-11-23 15:24:24 +0100

    Fix long loading no result
    Autocompletion waits to have a result before trying to display the list.
    If there were too many results on the API side, the load time (AJAX) was too long and nothing was displayed.
    More modern way (jQuery 1.8+) and cleaner.

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-jQuery(document).ready(function(){
-    jQuery("#address").keyup(function(){
-        if(jQuery(this).val() !== '') {
-            var getValue = encodeURIComponent(jQuery(this).val());
-            jQuery.ajax({
-                cache: false,
-                url: "https://api-adresse.data.gouv.fr/search/?q="+getValue+"&limit=5",
-                dataType: "json",
-                success: function(data){
-                    let labels = [];
+(function($) {
+    $(document).ready(function() {
+        var input = $("#address");
+
+        input.autocomplete({
+            source: function(request, response) {
+                $.ajax({
+                    url: "https://api-adresse.data.gouv.fr/search/?limit=5",
+                    data: {
+                        "q": input.val()
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        var labels = [];
                     data.features.forEach(feature => {
                         labels.push(feature.properties.label);
                     });
-                    
-                    jQuery("#address").autocomplete({
-                        source: function(request, response) {
-                            var matcher = new RegExp("^(?=.*\\b" + request.term.trim().split(/\s+/).join("\\b)(?=.*\\b") + ").*$", 'i' );
-                            response(jQuery.grep(labels, function(value) {
-				value = value.label || value.value || value;
-				return matcher.test(value) || matcher.test(value.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
-                            }));
-                        },
-                        minLength: 5,
-                        mustMatch: false
-                    });
-                }
-            });
-        }
+					console.log(labels);
+                        var matcher = new RegExp("^(?=.*\\b" + request.term.trim().split(/\s+/).join("\\b)(?=.*\\b") + ").*$", 'i');
+
+                        response($.grep(labels, function(value) {
+                            value = value.label || value.value || value;
+                            return matcher.test(value) || matcher.test(value.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
+                        }));
+                    }
+                });
+            },
+            minLength: 5,
+            mustMatch: false
+        });
+
     });
-});
+})(jQuery);
