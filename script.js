8f3c747beb143d2752eda7251380b9fbab07340e
Vincent Bourdon <vincent.bourdon41@gmail.com>
2022-11-23 16:14:59 +0100

    Remove regex check
    The regex has been removed as unnecessary and inconvenient for district research.

--- script.js
+++ script.js
@@ -15,13 +15,7 @@
                     data.features.forEach(feature => {
                         labels.push(feature.properties.label);
                     });
-					console.log(labels);
-                        var matcher = new RegExp("^(?=.*\\b" + request.term.trim().split(/\s+/).join("\\b)(?=.*\\b") + ").*$", 'i');
-
-                        response($.grep(labels, function(value) {
-                            value = value.label || value.value || value;
-                            return matcher.test(value) || matcher.test(value.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
-                        }));
+                        response(labels);
                     }
                 });
             },
